c57f7d6859805c18bc0324f6df7075a1efbd5858
seoksun.jang <boraseoksoon@gmail.com>
2021-12-30 21:33:27 -0500

    Add edge cases and test cases

--- Sources/Throttler/Throttler.swift
+++ Sources/Throttler/Throttler.swift
@@ -8,11 +8,13 @@
 import Foundation
 import Combine
 
-/// struct either throttling or debouncing successive works
+/// struct either throttling or debouncing successive works with provided options.
 public struct Throttler {
     
-    static private var workers: [String: Worker] = [:]
-    static private var debounceImmediatelyPerformed = false
+    typealias WorkIdentifier = String
+    
+    static private var workers: [WorkIdentifier: Worker] = [:]
+    
     /// Debounce a work
     ///
     ///     for i in 0...100000 {
@@ -23,51 +25,51 @@ public struct Throttler {
     ///
     ///     print("done!")
     ///
-    ///     "your work : 0"
-    ///     "your work : 100000"
-    ///     "done!"
+    ///     1. if shouldRunImmediately is true,
+    ///     => "your work : 0"
+    ///     otherwise, ignored.
+    ///     2. .... suppressing all the works, and the last
+    ///     3. "your work : 100000"
+    ///     4. "done!"
     ///
     /// - Note: Pay special attention to the identifier parameter. the default identifier is \("Thread.callStackSymbols") to make api trailing closure for one liner for the sake of brevity. However, it is highly recommend that a developer should provide explicit identifier for their work to debounce.
     ///
     /// - Parameters:
-    ///   - identifier: the identifier to group works to debounce. Throttler must have equivalent identifier to each work in a group to debounce in a group.
-    ///   - queue: queue to run the work on.
+    ///   - identifier: the identifier to group works to debounce. Throttler must have equivalent identifier to each work in a group to debounce.
+    ///   - queue: a queue to run the work on.
     ///   - delay: delay for debounce. time unit is second. given default is 1.0 sec.
-    ///   - shouldStartImmediately: A boolean type where true will run the first work immediately regardless.
-    ///   - work: the work to run
+    ///   - shouldRunImmediately: a boolean type where true will run the first work immediately regardless.
+    ///   - work: a work to run
     /// - Returns: Void
     static public func debounce(identifier: String = "\(Thread.callStackSymbols)",
                                 queue: DispatchQueue? = nil,
                                 delay: DispatchQueue.SchedulerTimeType.Stride = .seconds(1),
-                                shouldStartImmediately: Bool = true,
-                                work: @escaping () -> Void) {
+                                shouldRunImmediately: Bool = true,
+                                work: @escaping () -> Void)
+    {
         var worker: Worker? = nil
+        let isFirstRun = workers[identifier] == nil ? true : false
         
-        if let w = workers[identifier] {
+        if  let w = workers[identifier] {
             worker = w
         } else {
-            let w = Worker(queue: queue, delay: delay)
-            workers[identifier] = w
+            workers[identifier] = Worker(queue: queue, delay: delay)
             worker = workers[identifier]
         }
 
-        let canRunImmediately = shouldStartImmediately && !Self.debounceImmediatelyPerformed
-        
         worker!.deploy(
-            shouldStartImmediately:canRunImmediately,
-            work: work
+            work: work,
+            shouldRunImmediately:shouldRunImmediately && isFirstRun
         )
-        
-        if canRunImmediately {
-            Self.debounceImmediatelyPerformed = true
-        }
     }
 
     typealias Work = () -> Void
     typealias Subject = PassthroughSubject<Work, Never>?
     typealias Bag = Set<AnyCancellable>
-    static private var subjects: [String: (Subject, Bag)] = [:]
-    static private var throttleImmediatelyPerformed = false
+    
+    static private var subjects: [WorkIdentifier: Subject] = [:]
+    static private var bags: [WorkIdentifier: Bag] = [:]
+    
     /// Throttle a work
     ///
     ///     var sec = 0
@@ -99,31 +101,38 @@ public struct Throttler {
     /// - Note: Pay special attention to the identifier parameter. the default identifier is \("Thread.callStackSymbols") to make api trailing closure for one liner for the sake of brevity. However, it is highly recommend that a developer should provide explicit identifier for their work to debounce.
     ///
     /// - Parameters:
-    ///   - queue: queue to run the work on.
+    ///   - identifier: the identifier to group works to throttle. Throttler must have equivalent identifier to each work in a group to throttle.
+    ///   - queue: a queue to run the work on.
     ///   - delay: delay for throttle. time unit is second. given default is 1.0 sec.
-    ///   - shouldStartImmediately: A boolean type where true will run the first work immediately regardless.
-    ///   - work: the work to run
+    ///   - shouldRunImmediately: a boolean type where true will run the first work immediately regardless.
+    ///   - shouldRunLatest: A Boolean value that indicates whether to publish the most recent element. If `false`, the publisher emits the first element received during the interval.
+    ///   - work: a work to run
     /// - Returns: Void
     static public func throttle(identifier: String = "\(Thread.callStackSymbols)",
                                 queue: DispatchQueue? = nil,
                                 delay: DispatchQueue.SchedulerTimeType.Stride = .seconds(1),
-                                shouldStartImmediately: Bool = true,
+                                shouldRunImmediately: Bool = true,
                                 shouldRunLatest: Bool = true,
-                                work: @escaping () -> Void) {
-        let canRunImmediately = shouldStartImmediately && !Self.throttleImmediatelyPerformed
-        if canRunImmediately {
+                                work: @escaping () -> Void)
+    {
+        let isFirstRun = subjects[identifier] == nil ? true : false
+        
+        if shouldRunImmediately && isFirstRun {
             work()
-            Self.throttleImmediatelyPerformed = true
         }
         
-        if let (_, _) = subjects[identifier] {
-            subjects[identifier]?.0!.send(work)
+        if let _ = subjects[identifier] {
+            subjects[identifier]?!.send(work)
         } else {
-            subjects[identifier] = (PassthroughSubject<Work, Never>(), Bag())
-            subjects[identifier]?.0!
-                .throttle(for: delay, scheduler: queue ?? .global(), latest: shouldRunLatest)
+            subjects[identifier] = PassthroughSubject<Work, Never>()
+            bags[identifier] = Bag()
+            
+            let q = queue ?? .global()
+            
+            subjects[identifier]?!
+                .throttle(for: delay, scheduler: q, latest: shouldRunLatest)
                 .sink(receiveValue: { $0() })
-                .store(in: &subjects[identifier]!.1)
+                .store(in: &bags[identifier]!)
         }
     }
 
@@ -133,32 +142,30 @@ public struct Throttler {
         private let delay: DispatchQueue.SchedulerTimeType.Stride
         private var workItem: DispatchWorkItem?
 
-        fileprivate init(queue: DispatchQueue? = nil, delay: DispatchQueue.SchedulerTimeType.Stride) {
+        fileprivate init(queue: DispatchQueue? = nil,
+                         delay: DispatchQueue.SchedulerTimeType.Stride) {
             self.queue = queue
             self.delay = delay
         }
 
-        fileprivate func deploy(shouldStartImmediately: Bool, work: @escaping () -> Void) {
-            let canStartImmediately = shouldStartImmediately && workItem == nil
-            
-            if canStartImmediately {
+        fileprivate func deploy(work: @escaping () -> Void,
+                                shouldRunImmediately: Bool) {
+            if shouldRunImmediately && workItem == nil {
                 workItem = DispatchWorkItem(block: work)
                 work()
             } else {
-                debounce(work: work)
+                debounce(work)
             }
         }
 
-        private func debounce(work: @escaping () -> Void) {
-            let q = queue == nil ? DispatchQueue.global() : queue
-
+        private func debounce(_ work: @escaping () -> Void) {
             workItem?.cancel()
             workItem = DispatchWorkItem(block: work)
 
-            let deadline = DispatchTime.now() + delay.timeInterval
+            let q = queue == nil ? DispatchQueue.global() : queue
             
             q?.asyncAfter(
-                deadline: deadline,
+                deadline: DispatchTime.now() + delay.timeInterval,
                 execute: workItem!
             )
         }
--- Tests/ThrottlerTests/ThrottlerTests.swift
+++ Tests/ThrottlerTests/ThrottlerTests.swift
@@ -2,6 +2,198 @@ import XCTest
 @testable import Throttler
 
 final class ThrottlerTests: XCTestCase {
+    func test2() {
+    }
+    
+//    func test() {
+//        let interval: TimeInterval = 100.0
+//        let exp = expectation(description: "exp")
+//
+//        var sum = 0
+//        let count = 1000
+//        for i in 0...count {
+//            print("for loop : \(i)")
+//
+//            Throttler.throttle(
+//                delay:.seconds(0.01),
+//                shouldRunImmediately: false,
+//                shouldRunLatest: true
+//            ) {
+//                sum += 1
+//                print("sum : \(sum)")
+//            }
+//        }
+//
+//        waitForExpectations(timeout: interval)
+//    }
+    
+    func testDebounce() {
+        let interval: TimeInterval = 5.0
+        let exp = expectation(description: "exp")
+        
+        var sum = 0
+        let count = 5
+        for i in 0...count {
+            print("for loop : \(i)")
+            
+            Throttler.debounce(shouldRunImmediately: false) {
+                sum += 1
+                print("sum : \(sum)")
+
+                if i == count {
+                    exp.fulfill()
+                }
+            }
+        }
+
+//        for loop : 0
+//        for loop : 1
+//        for loop : 2
+//        for loop : 3
+//        for loop : 4
+//        for loop : 5
+//        sum : 1
+
+        waitForExpectations(timeout: interval)
+        XCTAssertEqual(sum, 1)
+        
+        print("***************************************")
+        print("***************************************")
+        print("***************************************")
+        print("***************************************")
+        
+        let exp2 = expectation(description: "exp2")
+        
+        var sum2 = 0
+        let count2 = 5
+        for i in 0...count2 {
+            print("for loop : \(i)")
+
+            Throttler.debounce {
+                sum2 += 1
+                print("sum2 : \(sum2)")
+                
+                if i == count {
+                    exp2.fulfill()
+                }
+            }
+            
+        }
+
+//        for loop : 0
+//        sum2 : 1
+//        for loop : 1
+//        for loop : 2
+//        for loop : 3
+//        for loop : 4
+//        for loop : 5
+//        sum2 : 2
+        
+        waitForExpectations(timeout: interval)
+        XCTAssertEqual(sum2, 2)
+    }
+    
+    func testThrottle() {
+        let interval: TimeInterval = 10.0
+        
+        let exp = expectation(description: "exp")
+        
+        var sum = 0
+        let count = 5
+        for i in 0...count {
+            print("for loop : \(i)")
+            
+            Throttler.throttle(
+                delay:.seconds(5.0),
+                shouldRunImmediately: false,
+                shouldRunLatest: false
+            ) {
+                sum += 1
+                print("sum : \(sum)")
+                
+                if sum == 2 {
+                    exp.fulfill()
+                }
+            }
+        }
+        
+//        for loop : 0
+//        for loop : 1
+//        for loop : 2
+//        sum : 1
+//        for loop : 3
+//        for loop : 4
+//        for loop : 5
+//        sum : 2
+
+        waitForExpectations(timeout: interval)
+        XCTAssertEqual(sum, 2)
+        
+        print("***************")
+        print("***************")
+        print("***************")
+        
+        let exp2 = expectation(description: "exp2")
+
+        var sum2 = 0
+        let count2 = 5
+        for i in 0...count2 {
+            print("for loop : \(i)")
+            
+            // shouldRunImmediately: true, shouldRunLatest: true by default.
+            Throttler.throttle {
+                sum2 += 1
+                print("sum2 : \(sum2)")
+                
+                if i == count2 {
+                    exp2.fulfill()
+                }
+            }
+        }
+
+//        for loop : 0
+//        sum2 : 1
+//        for loop : 1
+//        for loop : 2
+//        sum2 : 2
+//        for loop : 3
+//        for loop : 4
+//        for loop : 5
+//        sum2 : 3
+
+        waitForExpectations(timeout: interval)
+        XCTAssertEqual(sum2, 3)
+        
+        let exp3 = expectation(description: "exp2")
+
+        var sum3 = 0
+        let count3 = 5
+        for i in 0...count3 {
+            print("for loop : \(i)")
+            
+            // shouldRunImmediately: true, shouldRunLatest: true by default.
+            Throttler.throttle(shouldRunImmediately: false) {
+                sum3 += 1
+                print("sum3 : \(sum3)")
+                
+                if i == count2 {
+                    exp3.fulfill()
+                }
+            }
+        }
+        
+        //        for loop : 0
+        //        for loop : 1
+        //        for loop : 2
+        //        sum2 : 1
+        //        for loop : 3
+        //        for loop : 4
+        //        for loop : 5
+        //        sum2 : 2
+
+        waitForExpectations(timeout: interval)
+        XCTAssertEqual(sum3, 2)
+    }
     
     func testThrottlerShouldStartImmediately() {
         let interval: TimeInterval = 5.0
@@ -28,11 +220,17 @@ final class ThrottlerTests: XCTestCase {
         XCTAssertNotEqual(sum, count + 1)
             
         var sum2 = 0
-        Throttler.throttle {
+        Throttler.throttle(shouldRunImmediately:false) {
             sum2 += 1
-            
         }
-        XCTAssertNotEqual(sum2, 1)
+        XCTAssertEqual(sum2, 0)
+        
+        // shouldRunImmediately is true by default.
+        var sum22 = 0
+        Throttler.throttle {
+            sum22 += 1
+        }
+        XCTAssertEqual(sum22, 1)
         
         let exp3 = expectation(description: "exp3")
         var sum3 = 0
@@ -50,8 +248,7 @@ final class ThrottlerTests: XCTestCase {
 
         waitForExpectations(timeout: interval)
         XCTAssertEqual(sum3, 10)
-        
-        
+
         let exp4 = expectation(description: "exp4")
         let count4 = 10000
         var sum4 = 0
@@ -90,7 +287,7 @@ extension ThrottlerTests {
                 print("Debouncer.debounce1 executed: sum : \(sum)!")
             }
             
-            Throttler.debounce(queue: .global(), delay:2.0, shouldStartImmediately: false) {
+            Throttler.debounce(queue: .global(), delay:2.0, shouldRunImmediately: false) {
                 sum2 += 1
                 print("Debouncer.debounce2 executed: sum : \(sum)!")
                 
