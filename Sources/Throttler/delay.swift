f4cd602bc34977987a69a47afaa049fa82ef15b8
Jang Seoksoon <boraseoksoon@gmail.com>
2023-09-09 04:11:11 -0400

    Update delay.swift

--- Sources/Throttler/delay.swift
+++ Sources/Throttler/delay.swift
@@ -11,9 +11,14 @@ import Foundation
  Delays the execution of a provided operation by a specified time duration.
 
  - Parameters:
-   - duration: Foundation `Duration` type such sa .seconds(2.0). By default, .seconds(1.0)
+   - duration: Foundation `Duration` type such as .seconds(2.0). By default, .seconds(1.0)
    - actorType: The actor type on which the operation should be executed (default is `.main`).
    - operation: The operation to be executed after the delay.
+
+ - Note:
+   - The `delay` function is non-blocking, meaning it will not hold up the main thread (or the thread it's invoked on if you specify a different `actorType`).
+   - The delayed operation will be executed asynchronously, after the specified duration has passed.
+   - Use this function cautiously to avoid creating race conditions or other timing-related issues in your code.
 
  - Example:
 
@@ -28,7 +33,7 @@ import Foundation
         print("Another delayed operation")
     }
     ```
- */
+*/
 
 public func delay(
     _ duration: Duration = .seconds(1.0),
